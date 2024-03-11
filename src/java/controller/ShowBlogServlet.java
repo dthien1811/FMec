4fe2c013c5a38b9707c08cf9cf136d95c68705a4
Doanh9423 <156177526+dthien1811@users.noreply.github.com>
2024-03-11 16:23:28 +0700

    Update code blog

--- src/java/controller/ShowBlogServlet.java
+++ src/java/controller/ShowBlogServlet.java
@@ -7,11 +7,11 @@ package controller;
 import dal.BlogDAO;
 import dal.UserDAO;
 import entity.User;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
